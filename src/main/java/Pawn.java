0f708e7f0d9c6b2efe91f461013d865ecdfbae82
AleksandrKamen <Kamen.alexandr@mail.ru>
2023-10-09 15:12:32 +0300

    add Piece, BoardConsoleRenderer, Pawn, Queen, Coordinates clases

---
+++ src/main/java/Pawn.java
@@ -0,0 +1,7 @@
+
+public class Pawn extends Piece{
+
+    public Pawn(Color color, Coordinates coordinates) {
+        super(color, coordinates);
+    }
+}
---
+++ src/main/java/Piece.java
@@ -0,0 +1,10 @@
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Piece {
+    public final Color color;
+    public Coordinates coordinates;
+
+
+
+}
