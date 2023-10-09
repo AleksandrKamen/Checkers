6d470d1a69d9070ba3c3603ab91407e91325ba13
AleksandrKamen <Kamen.alexandr@mail.ru>
2023-10-09 21:22:13 +0300

    add setDefaultPiecesPosition method in Board, ANSI_Colors in BoardConsoleRenderer

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -12,4 +12,28 @@ public class Board {
         pieces.remove(coordinates);
     }
 
+    public void setDefaultPiecesPosition(){
+        for (File file : File.values()) {
+            if (file.ordinal() %2 ==0) {
+                setPiece(new Coordinates(file, 1), new Pawn(Color.White, new Coordinates(file,1)));
+                setPiece(new Coordinates(file, 3), new Pawn(Color.White, new Coordinates(file,3)));
+                setPiece(new Coordinates(file, 7), new Pawn(Color.Black, new Coordinates(file,7)));
+            } else {
+                setPiece(new Coordinates(file, 2), new Pawn(Color.White, new Coordinates(file,2)));
+                setPiece(new Coordinates(file, 6), new Pawn(Color.Black, new Coordinates(file,6)));
+                setPiece(new Coordinates(file, 8), new Pawn(Color.Black, new Coordinates(file,8)));
+            }
+        }
+    }
+
+    public  Piece getPiece(Coordinates coordinates){
+        return pieces.get(coordinates);
+    }
+    public boolean isSquareEmpty(Coordinates coordinates){
+        return !pieces.containsKey(coordinates);
+    } // Метод определения пустая ли клетка
+    public static boolean isSquareBlack(Coordinates coordinates) {
+        return (((coordinates.file.ordinal() + 1) + coordinates.rank) % 2) == 0;
+    }
+
 }
--- src/main/java/BoardConsoleRenderer.java
+++ src/main/java/BoardConsoleRenderer.java
@@ -1,2 +1,10 @@
 public class BoardConsoleRenderer {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_WHITE_PIECE_COLOR = "\u001B[97m";
+    public static final String ANSI_BLACK_PIECE_COLOR = "\u001B[30m";
+    public static final String ANSI_WHITE_SQUARE_BACKGROUND = "\u001B[47m";
+    public static final String ANSI_BLACK_SQUARE_BACKGROUND = "\u001B[0;100m";
+    public static final String ANSI_HIGHTLIGHTED_SQUAR_BACKGROUND = "\u001B[45m";
+
+
 }
