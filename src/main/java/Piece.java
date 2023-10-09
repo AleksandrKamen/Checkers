663e1e97f011114e15be7f51417fe0425f03c63b
AleksandrKamen <Kamen.alexandr@mail.ru>
2023-10-09 15:26:28 +0300

    add jacoco plugin, set depends test-jacoco, add constructor Piece

--- src/main/java/Piece.java
+++ src/main/java/Piece.java
@@ -1,10 +1,12 @@
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
+
+
 public class Piece {
     public final Color color;
     public Coordinates coordinates;
 
-
-
+    public Piece(Color color, Coordinates coordinates) {
+        this.color = color;
+        this.coordinates = coordinates;
+    }
 }
