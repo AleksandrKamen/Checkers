6c80c2733a70898f1b5a25361ee75bdb33a27a3d
AleksandrKamen <Kamen.alexandr@mail.ru>
2023-10-11 19:43:37 +0300

    add getCoordinatesBetweenSquare method in Board add unicod (count removePiece) in BoardConsoleRenderer

--- src/main/java/Board/Board.java
+++ src/main/java/Board/Board.java
@@ -1,73 +1,100 @@
 package Board;
 
 import Pieces.*;
+
 import java.util.*;
 
 
 public class Board {
+    private final int countPiecec = 12;
     HashMap<Coordinates, Piece> pieces = new HashMap<>();
     int WhiteRemoveCount = 0, BlackRemoveCount = 0;
 
-    public void setPiece(Coordinates coordinates, Piece piece){
+    public void setPiece(Coordinates coordinates, Piece piece) {
         piece.coordinates = coordinates;
-        pieces.put(coordinates,piece);
+        pieces.put(coordinates, piece);
     }
-    public void removePiece(Coordinates coordinates){
+
+    public void removePiece(Coordinates coordinates) {
         pieces.remove(coordinates);
     }
 
-    public void setDefaultPiecesPosition(){
+    public void setDefaultPiecesPosition() {
         for (File file : File.values()) {
-            if (file.ordinal() %2 ==0) {
-                setPiece(new Coordinates(file, 1), new Pawn(Color.White, new Coordinates(file,1)));
-                setPiece(new Coordinates(file, 3), new Pawn(Color.White, new Coordinates(file,3)));
-                setPiece(new Coordinates(file, 7), new Pawn(Color.Black, new Coordinates(file,7)));
+            if (file.ordinal() % 2 == 0) {
+                setPiece(new Coordinates(file, 1), new Pawn(Color.White, new Coordinates(file, 1)));
+                setPiece(new Coordinates(file, 3), new Pawn(Color.White, new Coordinates(file, 3)));
+                setPiece(new Coordinates(file, 7), new Pawn(Color.Black, new Coordinates(file, 7)));
             } else {
-                setPiece(new Coordinates(file, 2), new Pawn(Color.White, new Coordinates(file,2)));
-                setPiece(new Coordinates(file, 6), new Pawn(Color.Black, new Coordinates(file,6)));
-                setPiece(new Coordinates(file, 8), new Pawn(Color.Black, new Coordinates(file,8)));
+                setPiece(new Coordinates(file, 2), new Pawn(Color.White, new Coordinates(file, 2)));
+                setPiece(new Coordinates(file, 6), new Pawn(Color.Black, new Coordinates(file, 6)));
+                setPiece(new Coordinates(file, 8), new Pawn(Color.Black, new Coordinates(file, 8)));
             }
         }
     }
-    public  Piece getPiece(Coordinates coordinates){
+
+    public Piece getPiece(Coordinates coordinates) {
         return pieces.get(coordinates);
     }
-    public boolean isSquareEmpty(Coordinates coordinates){
+
+    public boolean isSquareEmpty(Coordinates coordinates) {
         return !pieces.containsKey(coordinates);
-    } // Метод определения пустая ли клетка
-    public  boolean isSquareBlack(Coordinates coordinates) {
+    }
+
+    public boolean isSquareBlack(Coordinates coordinates) {
         return (((coordinates.file.ordinal() + 1) + coordinates.rank) % 2) == 0;
     }
-    public boolean isWhitePiece(Piece piece){
+
+    public boolean isWhitePiece(Piece piece) {
         return piece.color == Color.White;
     }
 
-    public void setQueen(){
+    public void setQueen() {
         for (File file : File.values()) {
             Coordinates coordinatesW = new Coordinates(file, 8);
-            if (!isSquareEmpty(coordinatesW) && getPiece(coordinatesW).color == Color.White){
+            if (!isSquareEmpty(coordinatesW) && getPiece(coordinatesW).color == Color.White) {
                 removePiece(coordinatesW);
                 setPiece(coordinatesW, new Queen(Color.White, coordinatesW));
             }
-            Coordinates coordinatesB = new Coordinates(file,1);
-            if (!isSquareEmpty(coordinatesB) && getPiece(coordinatesB).color == Color.Black){
+            Coordinates coordinatesB = new Coordinates(file, 1);
+            if (!isSquareEmpty(coordinatesB) && getPiece(coordinatesB).color == Color.Black) {
                 removePiece(coordinatesB);
                 setPiece(coordinatesB, new Queen(Color.Black, coordinatesB));
             }
         }
     }
-    public void movePiece(Coordinates from, Coordinates to){
-        Piece piece =  getPiece(from);
+
+    public void movePiece(Coordinates from, Coordinates to) {
+        Piece piece = getPiece(from);
         removePiece(from);
         setPiece(to, piece);
+        getCoordinatesBetweenSquare(from, to).stream().forEach(c -> removePiece(c));
+        WhiteRemoveCount = countPiecec - getAllPiecesColor(Color.White).size();
+        BlackRemoveCount = countPiecec - getAllPiecesColor(Color.Black).size();
     }
 
-    public Set<Coordinates> getAllPiecesColor(Color color){
-        Set<Coordinates> PiecesCoordinates = new HashSet<>();
-        for (Map.Entry<Coordinates, Piece> pair : pieces.entrySet()){
+    public Set<Coordinates> getAllPiecesColor(Color color) {
+        var PiecesCoordinates = new HashSet<Coordinates>();
+        for (Map.Entry<Coordinates, Piece> pair : pieces.entrySet()) {
             Coordinates coordinates1 = pair.getKey();
-            if (!isSquareEmpty(coordinates1) && getPiece(coordinates1).color == color) PiecesCoordinates.add(coordinates1);
+            if (!isSquareEmpty(coordinates1) && getPiece(coordinates1).color == color)
+                PiecesCoordinates.add(coordinates1);
         }
         return PiecesCoordinates;
+    }
+
+    public Set<Coordinates> getCoordinatesBetweenSquare(Coordinates from, Coordinates to) {
+
+        Set<Coordinates> between = new HashSet<>();
+        int firstSquareFile = from.file.ordinal();
+        int firstSquareRank = from.rank;
+        int diff = Math.abs(to.file.ordinal() - firstSquareFile);
+        for (int b = 1; b < diff; b++) {
+            firstSquareFile += to.file.ordinal() - firstSquareFile > 0 ? 1 : -1;
+            firstSquareRank += to.rank - firstSquareRank > 0 ? 1 : -1;
+            between.add(new Coordinates(File.values()[firstSquareFile], firstSquareRank));
+        }
+        return between;
+
     }
 }
--- src/main/java/Board/BoardConsoleRenderer.java
+++ src/main/java/Board/BoardConsoleRenderer.java
@@ -10,29 +10,30 @@ public class BoardConsoleRenderer {
     private static final String ANSI_BLACK_SQUARE_BACKGROUND = "\u001B[0;100m";
 //    public static final String ANSI_HIGHTLIGHTED_SQUAR_BACKGROUND = "\u001B[45m";
 
-    public void render(Board board){
+    public void render(Board board) {
         // format = background color + font color + text + reset
-//        System.out.println("●".repeat());
+        System.out.println("● ".repeat(board.BlackRemoveCount));
         System.out.println("   A  B  C  D  E  F  G  H");
         for (int rank = 8; rank >= 1; rank--) {
             String line = "";
-            for(File file : File.values()){
+            for (File file : File.values()) {
                 Coordinates coordinates = new Coordinates(file, rank);
-                if (board.isSquareBlack(coordinates)){
-                    line += ANSI_BLACK_SQUARE_BACKGROUND +" ";
-                } else  line += ANSI_WHITE_SQUARE_BACKGROUND + " ";
+                if (board.isSquareBlack(coordinates)) {
+                    line += ANSI_BLACK_SQUARE_BACKGROUND + " ";
+                } else line += ANSI_WHITE_SQUARE_BACKGROUND + " ";
 
-                if (board.isSquareEmpty(coordinates)){
+                if (board.isSquareEmpty(coordinates)) {
                     line += "  ";
-                } else{
+                } else {
                     Piece piece = board.getPiece(coordinates);
-                    line += board.isWhitePiece(piece)? ANSI_WHITE_PIECE_COLOR + piece.getUnicod(): ANSI_BLACK_PIECE_COLOR + piece.getUnicod();
+                    line += board.isWhitePiece(piece) ? ANSI_WHITE_PIECE_COLOR + piece.getUnicod() : ANSI_BLACK_PIECE_COLOR + piece.getUnicod();
                 }
             }
-            System.out.printf("%d %s %1$d\n", rank, line  + ANSI_RESET);
+            System.out.printf("%d %s %1$d\n", rank, line + ANSI_RESET);
 
         }
         System.out.println("   A  B  C  D  E  F  G  H");
+        System.out.println(ANSI_WHITE_PIECE_COLOR + "● ".repeat(board.WhiteRemoveCount) + ANSI_RESET);
         System.out.println();
     }
 }
